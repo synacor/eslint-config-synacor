4549cdf3532c9f017876d749dcea977f1b4c5621
Jason Miller <developit@users.noreply.github.com>
2017-03-15 14:47:40 -0400

    single quotes

--- src/rules/tests.js
+++ src/rules/tests.js
@@ -1,4 +1,5 @@
 // Rules to use/override for unit/e2e tests
 export default {
-	"compat/compat": 2 //don't worry about browser compatibilty in our unit tests
+	//don't worry about browser compatibilty in our unit tests
+	'compat/compat': 2 
 };
